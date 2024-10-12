2cce9028527d416f39d5f52ff6d5ba14f7bf7242
Riley Flynn <riley@rileyflynn.me>
2024-10-12 15:47:44 -0230

    Hideous year selection UI

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,19 +1,36 @@
 import { Viewer } from '@/components/Viewer';
 import { useStore } from '@/lib/state';
 import { useGetYears } from '@/queries/api/skylineComponents';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 
 export function HomeRoute() {
     const { data: availableYears } = useGetYears({});
     const { user } = useStore();
+    const [selectedYear, setSelectedYear] = useState<number | undefined>();
 
     return (
-        <div className="h-dvh w-dvw">
-            {user && availableYears && (
-                <Suspense fallback={null}>
-                    <Viewer year={availableYears[0]} user={user} />
-                </Suspense>
-            )}
-        </div>
+        user &&
+        availableYears && (
+            <div className="flex h-dvh w-dvw flex-col">
+                <div className="flex-1">
+                    {selectedYear && (
+                        <Suspense fallback={null}>
+                            <Viewer key={selectedYear} year={selectedYear} user={user} />
+                        </Suspense>
+                    )}
+                </div>
+
+                <div className="h-fit">
+                    <select value={selectedYear} onChange={(e) => setSelectedYear(parseInt(e.target.value, 10))}>
+                        <option>Select a year</option>
+                        {availableYears.map((year) => (
+                            <option key={year} value={year}>
+                                {year}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+        )
     );
 }
