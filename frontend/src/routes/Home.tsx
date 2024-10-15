560873fb4516585faafae91dbe79380541bfc964
Riley Flynn <riley@rileyflynn.me>
2024-10-15 19:20:11 -0230

    WIP UI

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -2,7 +2,8 @@ import { Viewer } from '@/components/Viewer';
 import { GitHubIcon } from '@/icons/GitHub';
 import { useStore } from '@/lib/state';
 import { useGetYears } from '@/queries/api/skylineComponents';
-import { useState } from 'react';
+import { Button, Field, Input, Label, Select } from '@headlessui/react';
+import { type ChangeEvent, useState } from 'react';
 
 function LoginPrompt() {
     return (
@@ -19,28 +20,79 @@ function LoginPrompt() {
     );
 }
 
-export function HomeRoute() {
+function BottomBar({
+    selectedYear,
+    setSelectedYear,
+}: {
+    selectedYear: number | undefined;
+    setSelectedYear: (year: number | undefined) => void;
+}) {
     const { data: availableYears } = useGetYears({});
+
+    const [importYearSelection, setImportYearSelection] = useState<number | undefined>();
+
+    if (!availableYears) {
+        // TODO: Better loading UI
+        return null;
+    }
+
+    return (
+        <div className="flex h-fit flex-col items-center bg-zinc-800 p-4 md:grid-cols-2">
+            <div className="w-full space-y-4 md:w-1/3">
+                <Field className="flex flex-col gap-2">
+                    <Label className="font-semibold">Year</Label>
+                    <div className="flex gap-2">
+                        <Select
+                            className="flex-1 rounded-md bg-zinc-900 p-4"
+                            value={selectedYear}
+                            onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                                setSelectedYear(parseInt(e.target.value, 10) || undefined)
+                            }
+                        >
+                            <option>Select a year</option>
+                            {availableYears.map((year) => (
+                                <option key={year} value={year}>
+                                    {year}
+                                </option>
+                            ))}
+                        </Select>
+                        <Button className="w-1/3 rounded-md bg-emerald-600 p-4 transition-colors hover:bg-emerald-700">
+                            Download
+                        </Button>
+                    </div>
+                </Field>
+                <Field className="flex flex-col gap-2">
+                    <Label className="font-semibold">Import a year</Label>
+                    <div className="flex gap-2">
+                        <Input
+                            className="flex-1 rounded-md bg-zinc-900 p-4"
+                            type="number"
+                            min={2007}
+                            max={new Date().getFullYear() - 1}
+                            value={importYearSelection}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                                setImportYearSelection(parseInt(e.target.value, 10) || undefined)
+                            }
+                        />
+                        <Button className="w-1/3 rounded-md bg-violet-600 p-4 transition-colors hover:bg-violet-700">
+                            Import
+                        </Button>
+                    </div>
+                </Field>
+            </div>
+        </div>
+    );
+}
+
+export function HomeRoute() {
     const { user } = useStore();
     const [selectedYear, setSelectedYear] = useState<number | undefined>();
 
     return user ? (
-        availableYears && (
-            <div className="flex h-dvh w-dvw flex-col">
-                <div className="flex-1">{selectedYear && <Viewer year={selectedYear} user={user} />}</div>
-
-                <div className="h-fit">
-                    <select value={selectedYear} onChange={(e) => setSelectedYear(parseInt(e.target.value, 10))}>
-                        <option>Select a year</option>
-                        {availableYears.map((year) => (
-                            <option key={year} value={year}>
-                                {year}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </div>
-        )
+        <div className="flex h-dvh w-dvw flex-col">
+            <div className="flex-1">{selectedYear && <Viewer year={selectedYear} user={user} />}</div>
+            <BottomBar selectedYear={selectedYear} setSelectedYear={setSelectedYear} />
+        </div>
     ) : (
         <LoginPrompt />
     );
