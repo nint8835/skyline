45243994e1df5303f02d72869ef7549e40e84963
Riley Flynn <riley@rileyflynn.me>
2024-10-19 19:17:04 -0230

    Gump's stuff, but working better

--- frontend/src/components/Viewer.tsx
+++ frontend/src/components/Viewer.tsx
@@ -1,8 +1,8 @@
 import { useStore } from '@/lib/state';
-import { Bounds, CameraControls, Center, useBounds } from '@react-three/drei';
+import { CameraControls, Environment } from '@react-three/drei';
 import { Canvas, useLoader } from '@react-three/fiber';
-import { Suspense, useEffect, useRef, useState } from 'react';
-import { Mesh } from 'three';
+import { Suspense, useEffect, useRef } from 'react';
+import { Group, Sphere } from 'three';
 import { STLLoader } from 'three-stdlib';
 
 function LoadingIndicator() {
@@ -14,52 +14,42 @@ function LoadingIndicator() {
 }
 
 function Model() {
-    const [hasBound, setHasBound] = useState(false);
     const { selectedYear } = useStore();
-    const bounds = useBounds();
-
     const modelUrl = selectedYear ? `/contributions/model/${selectedYear}` : '/blank.stl';
-
     const stl = useLoader(STLLoader, modelUrl);
-    const meshRef = useRef<Mesh>(null);
+    const camControlsRef = useRef<CameraControls>(null);
+    const groupRef = useRef<Group>(null);
 
     useEffect(() => {
-        if (!stl || hasBound || !meshRef.current) {
-            return;
+        if (camControlsRef.current) {
+            console.log('Fitting');
+            stl.computeBoundingSphere();
+            camControlsRef.current.fitToSphere(stl.boundingSphere as Sphere, false);
         }
-        // bounds.moveTo([0, 100, 100]);
-        // bounds.refresh().clip().fit();
-        setHasBound(true);
-    }, [stl]);
+    }, [camControlsRef, stl]);
 
     return (
-        <mesh
-            geometry={stl}
-            castShadow
-            receiveShadow
-            rotation={[-Math.PI / 2, 0, -Math.PI / 2]}
-            position={[-78, 0, 0]}
-            ref={meshRef}
-        >
-            <meshStandardMaterial />
-        </mesh>
+        <group ref={groupRef}>
+            <mesh
+                geometry={stl}
+                castShadow
+                receiveShadow
+                rotation={[-Math.PI / 2, 0, -Math.PI / 2]}
+                position={[-78, 0, 0]}
+            >
+                <meshStandardMaterial />
+            </mesh>
+            <CameraControls ref={camControlsRef} makeDefault />
+        </group>
     );
 }
 
 function ViewerCanvas() {
     return (
         <Canvas shadows>
-            {/* <PerspectiveCamera makeDefault /> */}
-            {/* <OrbitControls makeDefault /> */}
-            <CameraControls makeDefault />
-            <Bounds>
-                <Center>
-                    <Model />
-                </Center>
-            </Bounds>
-
-            <ambientLight intensity={0.25} />
-            <directionalLight position={[-1, 5, -1]} intensity={1} />
+            <Model />
+            <ambientLight />
+            <Environment preset="sunset" />
         </Canvas>
     );
 }
