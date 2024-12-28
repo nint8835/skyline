9419b720a77264beba5747fab8205261d7680281
Riley Flynn <riley@rileyflynn.me>
2024-12-28 19:14:14 -0330

    Text performance improvement attempt 3 (#7)
    * Revert "Revert "Text performance improvement attempt 2 (#5)" (#6)"
    
    This reverts commit 3ca0cc31d2176c1449a279bfb0ae222e4d3260ba.
    
    * Hopefully finally fix it this time

--- skyline/cad/skyline.py
+++ skyline/cad/skyline.py
@@ -1,6 +1,7 @@
+import math
 import tempfile
 from pathlib import Path
-from typing import Sequence, cast
+from typing import Self, Sequence, cast
 
 import cadquery
 
@@ -10,33 +11,67 @@ GRID_BASE_HEIGHT = 2
 INTER_FONT_PATH = str(Path(__file__).parent / "fonts" / "Inter-Regular.ttf")
 
 
+class PendingPolyline:
+    def __init__(self, offset_x: int | float = 0, offset_y: int | float = 0):
+        self.points: list[tuple[int | float, int | float]] = []
+        self.offset_x = offset_x
+        self.offset_y = offset_y
+
+    def push(self, x: int | float, y: int | float) -> Self:
+        x += self.offset_x
+        y += self.offset_y
+
+        # Do not push a point into the polyline if it is as the same location as the previous point
+        # Doing so will result in a "BRep_API: command not done" error
+        if len(self.points) > 0 and self.points[-1] == (x, y):
+            return self
+
+        self.points.append((x, y))
+        return self
+
+
 def skyline_model(
     *,
     days: Sequence[int | None],
     label: str | None = "",
     include_month_label: bool = True,
 ) -> bytes:
-    grid = cadquery.Assembly()
+    cols = math.floor(len(days) / 7.0)
+    first_day_row = days.index(next(day for day in days if day is not None))
+    # First week is None-padded, but last week is not None-padded
+    # Therefore, to get the row of the last day we use the length of the last week
+    last_day_row = len(days[7 * cols :]) - 1
+    center_row_offset = -GRID_SQUARE_SIZE * 7 / 2
+    center_col_offset = -GRID_SQUARE_SIZE * cols / 2
 
-    for index, count in enumerate(days):
-        col_offset = (-GRID_SQUARE_SIZE * 52 / 2) + GRID_SQUARE_SIZE * (index // 7)
-        row_offset = (-GRID_SQUARE_SIZE * 7 / 2) + GRID_SQUARE_SIZE * (index % 7)
+    base_polyline = PendingPolyline(center_row_offset, center_col_offset)
 
-        if count is None:
-            continue
-
-        grid.add(
-            cadquery.Workplane("XY").box(
-                GRID_SQUARE_SIZE,
-                GRID_SQUARE_SIZE,
-                count + GRID_BASE_HEIGHT,
-                centered=False,
-            ),
-            loc=cadquery.Location(cadquery.Vector(row_offset, col_offset, 0)),
-        )
+    #     C─────────────────────────────┬───D
+    #     │                             │   │
+    # A───B                             │   │
+    # │   │                             │   │
+    # │   │                             │   │
+    # │   │                             │   │
+    # │   │                             F───E
+    # │   │                             │
+    # H───┴─────────────────────────────G
+    (
+        base_polyline.push(first_day_row * GRID_SQUARE_SIZE, 0)  # A
+        .push(first_day_row * GRID_SQUARE_SIZE, GRID_SQUARE_SIZE)  # B
+        .push(0, GRID_SQUARE_SIZE)  # C
+        .push(0, (cols + 1) * GRID_SQUARE_SIZE)  # D
+        .push((last_day_row + 1) * GRID_SQUARE_SIZE, (cols + 1) * GRID_SQUARE_SIZE)  # E
+        .push((last_day_row + 1) * GRID_SQUARE_SIZE, cols * GRID_SQUARE_SIZE)  # F
+        .push(7 * GRID_SQUARE_SIZE, cols * GRID_SQUARE_SIZE)  # G
+        .push(7 * GRID_SQUARE_SIZE, 0)  # H
+        .push(first_day_row * GRID_SQUARE_SIZE, 0)  # A
+    )
 
     skyline_workplane = (
-        cadquery.Workplane("XY").center(0, 0).workplane().add(grid.toCompound())
+        cadquery.Workplane("XY")
+        .polyline(base_polyline.points)
+        .close()
+        .extrude(GRID_BASE_HEIGHT)
     )
 
     if label:
@@ -59,7 +94,9 @@ def skyline_model(
             skyline_workplane.faces("<Z")
             .workplane()
             # TODO: Use a better way to position the label rather than a hardcoded offset
-            .transformed(offset=cadquery.Vector(0, GRID_SQUARE_SIZE * 25, 0))
+            .transformed(
+                offset=cadquery.Vector(0, -1 * center_col_offset - GRID_SQUARE_SIZE, 0)
+            )
             .text(  # type: ignore - Decorator on .text currently breaks typing. PR opened to resolve this (https://github.com/CadQuery/cadquery/pull/1733)
                 "Jan",
                 7.5,
@@ -69,7 +106,7 @@ def skyline_model(
             )
             # TODO: Use a better way to position the label rather than a hardcoded offset
             .transformed(
-                offset=cadquery.Vector(0, GRID_SQUARE_SIZE * -51, 0),
+                offset=cadquery.Vector(0, 2 * center_col_offset + GRID_SQUARE_SIZE, 0),
                 rotate=cadquery.Vector(0, 0, 180),
             )
             .text(  # type: ignore - Decorator on .text currently breaks typing. PR opened to resolve this (https://github.com/CadQuery/cadquery/pull/1733)
@@ -80,6 +117,29 @@ def skyline_model(
                 valign="top",
             ),
         )
+
+    grid = cadquery.Assembly()
+
+    for index, count in enumerate(days):
+        col_offset = center_col_offset + GRID_SQUARE_SIZE * (index // 7)
+        row_offset = center_row_offset + GRID_SQUARE_SIZE * (index % 7)
+
+        if not count:
+            continue
+
+        grid.add(
+            cadquery.Workplane("XY").box(
+                GRID_SQUARE_SIZE,
+                GRID_SQUARE_SIZE,
+                count,
+                centered=False,
+            ),
+            loc=cadquery.Location(
+                cadquery.Vector(row_offset, col_offset, GRID_BASE_HEIGHT)
+            ),
+        )
+
+    skyline_workplane = skyline_workplane.add(grid.toCompound())
 
     with tempfile.TemporaryDirectory(prefix="skyline_") as tmpdir:
         skyline_workplane.export(f"{tmpdir}/skyline.stl")
