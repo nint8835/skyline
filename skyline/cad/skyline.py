f93f47c102cca1585f2cbc536831977f767d9d3b
Riley Flynn <riley@rileyflynn.me>
2024-12-28 17:31:24 -0330

    Revert "Text performance improvement (#3)"
    This reverts commit a2f0942b4c6504da23e0953f294d95e0b6f9c177.

--- skyline/cad/skyline.py
+++ skyline/cad/skyline.py
@@ -1,7 +1,6 @@
-import math
 import tempfile
 from pathlib import Path
-from typing import Self, Sequence, cast
+from typing import Sequence, cast
 
 import cadquery
 
@@ -11,67 +10,33 @@ GRID_BASE_HEIGHT = 2
 INTER_FONT_PATH = str(Path(__file__).parent / "fonts" / "Inter-Regular.ttf")
 
 
-class PendingPolyline:
-    def __init__(self, local_offset: tuple[int | float, int | float] = (0, 0)):
-        self.points: list[tuple[int | float, int | float]] = []
-        self.local_offset = local_offset
-
-    def push(self, x: int | float, y: int | float) -> Self:
-        x += self.local_offset[0]
-        y += self.local_offset[1]
-
-        # Do not push a point into the polyline if it is as the same location as the previous point
-        # Doing so will result in a "BRep_API: command not done" error
-        if len(self.points) > 0 and self.points[-1] == (x, y):
-            return self
-
-        self.points.append((x, y))
-        return self
-
-
 def skyline_model(
     *,
     days: Sequence[int | None],
     label: str | None = "",
     include_month_label: bool = True,
 ) -> bytes:
-    cols = math.ceil(len(days) / 7.0)
-    first_day_row = days.index(next(day for day in days if day is not None))
-    # First week is None-padded, but last week is not None-padded
-    # Therefore, to get the row of the last day we use the length of the last week
-    last_day_row = len(days[7 * (cols - 1) :]) - 1
-    center_row_offset = -GRID_SQUARE_SIZE * 7 / 2
-    center_col_offset = -GRID_SQUARE_SIZE * cols / 2
+    grid = cadquery.Assembly()
 
-    base_polyline = PendingPolyline((center_row_offset, center_col_offset))
+    for index, count in enumerate(days):
+        col_offset = (-GRID_SQUARE_SIZE * 52 / 2) + GRID_SQUARE_SIZE * (index // 7)
+        row_offset = (-GRID_SQUARE_SIZE * 7 / 2) + GRID_SQUARE_SIZE * (index % 7)
 
-    (
-        base_polyline
-        # First week, top left
-        .push(first_day_row * GRID_SQUARE_SIZE, 0)
-        # First week, top right
-        .push(first_day_row * GRID_SQUARE_SIZE, GRID_SQUARE_SIZE)
-        # Second week, top left
-        .push(0, GRID_SQUARE_SIZE)
-        # Last week, top right
-        .push(0, (cols + 1) * GRID_SQUARE_SIZE)
-        # Last week, bottom right
-        .push((last_day_row + 1) * GRID_SQUARE_SIZE, (cols + 1) * GRID_SQUARE_SIZE)
-        # Last week, bottom left
-        .push((last_day_row + 1) * GRID_SQUARE_SIZE, cols * GRID_SQUARE_SIZE)
-        # Second last week, bottom right
-        .push(7 * GRID_SQUARE_SIZE, cols * GRID_SQUARE_SIZE)
-        # First week, bottom left
-        .push(7 * GRID_SQUARE_SIZE, 0)
-        # # First week, top left
-        .push(first_day_row * GRID_SQUARE_SIZE, 0)
-    )
+        if count is None:
+            continue
+
+        grid.add(
+            cadquery.Workplane("XY").box(
+                GRID_SQUARE_SIZE,
+                GRID_SQUARE_SIZE,
+                count + GRID_BASE_HEIGHT,
+                centered=False,
+            ),
+            loc=cadquery.Location(cadquery.Vector(row_offset, col_offset, 0)),
+        )
 
     skyline_workplane = (
-        cadquery.Workplane("XY")
-        .polyline(base_polyline.points)
-        .close()
-        .extrude(GRID_BASE_HEIGHT)
+        cadquery.Workplane("XY").center(0, 0).workplane().add(grid.toCompound())
     )
 
     if label:
@@ -115,29 +80,6 @@ def skyline_model(
                 valign="top",
             ),
         )
-
-    grid = cadquery.Assembly()
-
-    for index, count in enumerate(days):
-        col_offset = (-GRID_SQUARE_SIZE * 52 / 2) + GRID_SQUARE_SIZE * (index // 7)
-        row_offset = (-GRID_SQUARE_SIZE * 7 / 2) + GRID_SQUARE_SIZE * (index % 7)
-
-        if not count:
-            continue
-
-        grid.add(
-            cadquery.Workplane("XY").box(
-                GRID_SQUARE_SIZE,
-                GRID_SQUARE_SIZE,
-                count,
-                centered=False,
-            ),
-            loc=cadquery.Location(
-                cadquery.Vector(row_offset, col_offset, GRID_BASE_HEIGHT)
-            ),
-        )
-
-    skyline_workplane = skyline_workplane.add(grid.toCompound())
 
     with tempfile.TemporaryDirectory(prefix="skyline_") as tmpdir:
         skyline_workplane.export(f"{tmpdir}/skyline.stl")
